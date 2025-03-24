02ed2f16121297602354a23a2b04af47e08324ca
baydouna <ali.baydoun@etu.univ-grenoble-alpes.fr>
2025-03-24 17:25:21 +0100

    keyboard i/o

---
+++ step1/uart-mmio.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright: Olivier Gruber (olivier dot gruber at acm dot org)
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef UART_MMIO_H_
+#define UART_MMIO_H_
+
+/**
+ * To fill this header file,
+ * look at the document describing the Versatile Application Board:
+ *
+ *    Versatile Application Baseboard for ARM926EJ-S (DUI0225)
+ */
+
+/*
+ * We need the base address for the different serial lines.
+ */
+
+#define UART0_BASE_ADDRESS ((void *)0x101F1000)
+#define UART1_BASE_ADDRESS ((void *)0x101F2000)
+#define UART2_BASE_ADDRESS ((void *)0x101F3000)
+
+/*
+ * Register offsets for the PL011 UART
+ */
+#define UART_DR 0x000   /* Data Register (Read/Write, TX/RX FIFO) */
+#define UART_FR 0x018   /* Flag Register (Read-Only, status flags) */
+#define UART_IMSC 0x038 /* Interrupt Mask Set/Clear Register */
+#define UART_RIS 0x03C  /* Raw Interrupt Status Register */
+#define UART_MIS 0x040  /* Masked Interrupt Status Register */
+#define UART_ICR 0x044  /* Interrupt Clear Register */
+
+/*
+ * Flag Register (UARTFR) bit masks
+ */
+#define UART_FR_TXFF (1 << 5) /* Transmit FIFO full */
+#define UART_FR_RXFE (1 << 4) /* Receive FIFO empty */
+#define UART_FR_TXFE (1 << 7) /* Transmit FIFO empty */
+#define UART_FR_BUSY (1 << 3) /* UART is busy transmitting */
+
+/*
+ * Interrupt Mask Set/Clear Register (UARTIMSC)  bit masks
+ */
+#define UART_IMSC_RXIM (1 << 4) /* Receive Interrupt Mask */
+#define UART_IMSC_TXIM (1 << 5) /* Transmit Interrupt Mask */
+
+#endif /* UART_MMIO_H_ */
